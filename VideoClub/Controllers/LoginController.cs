56a53615d8f816c9237ec6f69d310196a1b82a5f
Juan Marcos Susana Feliz <mikesusana14@gmail.com>
2018-08-08 11:39:41 -0400

    GIT INIT x2

---
+++ VideoClub/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VideoClub.Models;
+
+namespace VideoClub.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public bool login(string username, string password)
+        {
+            using (var db = new VideoClubEntities())
+            {
+                var user = db.Employees.FirstOrDefault(e => e.User == username);
+
+                if (user != null)
+                {
+                    return (user.Password == password);
+                }
+
+            }
+
+            return false;
+        }
+    }
+}
